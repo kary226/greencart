1b1e88bdeda03b12e3cf0303be00a118e67fafe4
kary226 <kary226@gmail.com>
2026-05-22 19:06:43 +0000

    Mise à jour

--- server/middlewares/authUser.js
+++ server/middlewares/authUser.js
@@ -1,24 +1,26 @@
 import jwt from 'jsonwebtoken';
 
-const authUser = async (req, res, next)=>{
-    const {token} = req.cookies;
-
-    if(!token){
-        return res.json({ success: false, message: 'Not Authorized' });
+const authUser = async (req, res, next) => {
+    // Récupérer le token depuis le header Authorization
+    const authHeader = req.headers.authorization;
+    
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.json({ success: false, message: 'Not Authorized - Token manquant' });
     }
+    
+    const token = authHeader.split(' ')[1];
 
     try {
-        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET)
-        if(tokenDecode.id){
+        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+        if (tokenDecode.id) {
             req.body.userId = tokenDecode.id;
-        }else{
-            return res.json({ success: false, message: 'Not Authorized' });
+        } else {
+            return res.json({ success: false, message: 'Not Authorized - Token invalide' });
         }
         next();
-
     } catch (error) {
         res.json({ success: false, message: error.message });
     }
-}
+};
 
 export default authUser;
