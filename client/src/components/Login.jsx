2f8abb718af3ef4cb6e62a08f4d1ec75f892ef03
kary226 <kary226@gmail.com>
2026-05-23 18:25:20 +0000

    Mise à jour

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -38,7 +38,7 @@ const Login = () => {
 
             <form onSubmit={onSubmitHandler} onClick={(e) => e.stopPropagation()} className="flex flex-col gap-4 m-auto items-start p-8 py-12 w-80 sm:w-[352px] rounded-lg shadow-xl border border-gray-200 bg-white">
                 <p className="text-2xl font-medium m-auto">
-                    <span className="text-primary">Utilisateur</span> {state === "login" ? "Login" : "Sign Up"}
+                    <span className="text-primary">GreenCart</span> {state === "login" ? "Connexion" : "Inscription"}
                 </p>
                 {state === "register" && (
                     <>
@@ -54,20 +54,20 @@ const Login = () => {
                 )}
                 <div className="w-full">
                     <p>Email</p>
-                    <input onChange={(e) => setEmail(e.target.value)} value={email} placeholder="type here" className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary" type="email" required />
+                    <input onChange={(e) => setEmail(e.target.value)} value={email} placeholder="exemple@email.com" className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary" type="email" required />
                 </div>
                 <div className="w-full">
                     <p>Mot de passe</p>
-                    <input onChange={(e) => setPassword(e.target.value)} value={password} placeholder="type here" className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary" type="password" required />
+                    <input onChange={(e) => setPassword(e.target.value)} value={password} placeholder="Votre mot de passe" className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary" type="password" required />
                 </div>
                 {state === "register" ? (
                     <p>
-                        J'ai déja un compte? <span onClick={() => setState("login")} className="text-primary cursor-pointer">click here</span>
+                        J'ai déjà un compte <span onClick={() => setState("login")} className="text-primary cursor-pointer">Se connecter</span>
                     </p>
                 ) : (
                     <>
                         <p>
-                            Créer un compte? <span onClick={() => setState("register")} className="text-primary cursor-pointer">click here</span>
+                        Vous n'avez pas de compte ? <span onClick={() => setState("register")} className="text-primary cursor-pointer">Créer un compte</span>
                         </p>
                         <Link
                             to="/forgot-password"
@@ -79,7 +79,7 @@ const Login = () => {
                     </>
                 )}
                 <button disabled={loading} className="bg-primary hover:bg-primary-dull transition-all text-white w-full py-2 rounded-md cursor-pointer disabled:opacity-50">
-                    {loading ? "Chargement..." : (state === "register" ? "Create Account" : "Login")}
+                    {loading ? "Chargement..." : (state === "register" ? "S'inscrire" : "Se connecter")}
                 </button>
             </form>
         </div>
