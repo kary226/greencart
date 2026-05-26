df8a947d4483b9ff0f5738946e9098bad22e6336
kary226 <kary226@gmail.com>
2026-05-26 00:23:04 +0000

    Mise à jour

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -4,12 +4,12 @@ import authUser from '../middlewares/authUser.js';
 
 const userRouter = express.Router();
 
-userRouter.post('/register', register)
-userRouter.post('/login', login)
-userRouter.get('/is-auth', authUser, isAuth)
-userRouter.get('/logout', authUser, logout)
-userRouter.put('/update', authUser, updateUser)
-userRouter.post('/forgot-password', forgotPassword)
-userRouter.post('/reset-password', resetPassword)
+userRouter.post('/register', register);
+userRouter.post('/login', login);
+userRouter.get('/is-auth', authUser, isAuth);
+userRouter.post('/logout', authUser, logout);  // ← POST au lieu de GET
+userRouter.post('/update', authUser, updateUser);  // ← POST au lieu de PUT
+userRouter.post('/forgot-password', forgotPassword);
+userRouter.post('/reset-password', resetPassword);
 
-export default userRouter
+export default userRouter;
