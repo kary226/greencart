f44b6495dcf9506cb4423f95dbe164411758292e
kary226 <kary226@gmail.com>
2026-07-03 23:14:18 +0000

    Mise à jour

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -230,7 +230,7 @@ export default function MyOrders() {
                                                 padding: '2px 10px',
                                                 borderRadius: 20,
                                             }}>
-                                                📦 Livraison prévue entre le {deliveryStart} et le {deliveryEnd}
+                                                 Livraison prévue entre le {deliveryStart} et le {deliveryEnd}
                                             </span>
                                         )}
                                     </div>
